f69eb9887b5b808858f624749376bb19e98188f3
Pravesh-mansharamani <praveshramani295@gmail.com>
2023-09-01 16:28:51 +0530

    first commit

---
+++ src/assets/protein/index.js
@@ -0,0 +1,58 @@
+import bestProduct1 from './on-whey.jpg'
+import bestProduct2 from './muscletech.jpg'
+import bestProduct3 from './dymatize-elite.jpeg' 
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import Logo from './lifestyle-logo.png'
+import footerLogo from './footerLogo.png'
+import navLogo from './navLogo2.png'
+
+import offer from './offer.svg'
+
+import product4 from './myprotein.webp'
+import product5 from './dynamite-iso.webp'
+import product6 from './gnc-pro.webp'
+import product7 from './gnc-pure.jpeg'
+import product8 from './iso-sensation.webp'
+import product9 from './isopure.webp'
+import product10 from './muscleblaze-bio.webp'
+import product11 from './muscleblaze-gold.webp'
+import product12 from './on-isolate.webp'
+import product13 from './syntha.webp'
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailProduct1 from './on-whey-removebg-preview.png'
+import thumbnailProduct2 from './muscletech-removebg-preview.png'
+import thumbnailProduct3 from './dymatize-elite-removebg-preview.png'
+
+export {
+    bestProduct1,
+    bestProduct2,
+    bestProduct3,
+
+    customer1,
+    customer2,
+
+    Logo,
+    footerLogo,
+    navLogo,
+
+    offer,
+    product4,
+    product5,
+    product6,
+    product7,
+    product8,
+    product9,
+    product10,
+    product11,
+    product12,
+    product13,
+
+    thumbnailBackground,
+    thumbnailProduct1,
+    thumbnailProduct2,
+    thumbnailProduct3
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { navLogo } from "../assets/protein"
+import { hamburger } from "../assets/icons"
+import { navLinks } from "../constants"
+
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+        <nav className="flex justify-between items-center max-container">
+            <a href="/">
+                <img src={navLogo} alt="/" width={139} height={49} className="m-0 w-[139px] h-[49px]"/>
+            </a>
+            <ul className="flex-1 flex justify-center item-center gap-16 max-lg:hidden">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="hidden max-lg:block">
+                <img src={hamburger} alt="hamburger icon" width={25} height={25} />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
