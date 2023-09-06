4fdbdd6317ac21472fe3fc1720b3060d70c17798
Pravesh-mansharamani <praveshramani295@gmail.com>
2023-09-06 19:19:54 -0400

    completed the structure

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,11 @@
 import { shoes, statistics } from "../constants";
-// import { Button, ShoeCard } from "../components";
+import { Button, ShoeCard } from "../components";
 import { useState } from "react";
 import { arrowRight } from "../assets/icons";
 import { bestProduct1 } from "../assets/protein";
 
 const Hero = () => {
-  const [bestProductImg, setBestProductImg] = useState(bestProduct1);
+  const [bestProductImg, setBestProductImg] = useState(bestProduct1); 
 
   return (
     <section
@@ -13,25 +13,25 @@ const Hero = () => {
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
-        <p className='text-xl font-montserrat text-coral-red'>
+        <p className='text-xl font-montserrat text-sky-500'>
           Our Best Selling Products
         </p>
 
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-            The New Arrival
+            The
           </span>
           <br />
-          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+          <span className='text-sky-500 inline-block mt-3'>LifeStyle</span> Nutrition
         </h1>
-        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-          Discover stylish Nike arrivals, quality comfort, and innovation for
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-7 sm:max-w-sm'>
+          Discover all your quality, comfort, and innovation products for
           your active life.
         </p>
 
-        {/* <Button label='Shop now' iconURL={arrowRight} /> */}
+        <Button label='Shop now' iconURL={arrowRight} />
 
-        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+        <div className='flex justify-start items-start flex-wrap w-full mt-3 gap-16'>
           {statistics.map((stat, index) => (
             <div key={index}>
               <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
@@ -43,24 +43,24 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-hero bg-cover bg-center '>
         <img
           src={bestProductImg}
           alt='shoe colletion'
-          width={610}
-          height={502}
+          width={510}
+          height={402}
           className='object-contain relative z-10'
         />
 
         <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
           {shoes.map((image, index) => (
             <div key={index}>
-              {/* <ShoeCard
+              <ShoeCard
                 index={index}
                 imgURL={image}
                 changeBigShoeImage={(shoe) => setBestProductImg(shoe)}
                 bigShoeImg={bestProductImg}
-              /> */}
+              />
             </div>
           ))}
         </div>
